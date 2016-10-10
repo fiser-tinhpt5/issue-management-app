f4a27ebe267ea4936fd2ae00acc673157128db43
Pham Trung Tinh <tinh_pt@septeni-technology.jp>
2016-10-10 17:24:53 +0700

    Create form update

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.Json
 /**
  * Created by septechuser on 07/10/2016.
  */
-case class User(userName: String, password: String)
+case class User(id: Int, userName: String, password: String)
 
 object User {
   implicit val userFormat = Json.format[User]
