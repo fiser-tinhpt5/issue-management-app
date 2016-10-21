acd9a36fda70b237e1e56fdf45bd122c5d5d17b4
Pham Trung Tinh <tinh_pt@septeni-technology.jp>
2016-10-21 16:24:39 +0700

    DONE

--- test/issue/controller/IssueControllerSpec.scala
+++ test/issue/controller/IssueControllerSpec.scala
@@ -3,6 +3,7 @@ package issue.controller
 import java.util.Date
 
 import controllers.issue.IssueController
+import models.issue.Status.NOT_YET
 import models.issue.{ Issue, IssueDAO }
 import org.specs2.mock.Mockito
 import play.api.test.{ FakeRequest, PlaySpecification, WithApplication }
@@ -17,7 +18,7 @@ class IssueControllerSpec extends PlaySpecification with Mockito {
 
   def issueControllerWithMock(mockIssueDAO: IssueDAO) = new IssueController(mockIssueDAO)
 
-  val dummyIssue = Issue(1, "write test code for controllers", "know nothing about mock", new Date(), "NOT YET")
+  val dummyIssue = Issue(1, "write test code for controllers", "know nothing about mock", new Date(), NOT_YET)
 
   "IssueController " should {
     "list " should {
